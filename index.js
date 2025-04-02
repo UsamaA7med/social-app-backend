5a8f7599a71f7965f5d88ddc29855f29620d16d7
UsamaA7med <osamaahmedwork1@gmail.com>
2025-04-02 19:48:17 +0200

    change the origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   corse({
-    origin: process.env.CLIENT_URL,
+    origin: "https://social-app-frontend-tawny.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
