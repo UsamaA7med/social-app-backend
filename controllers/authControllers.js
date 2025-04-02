25c57b1d0196da9604f7c6bd38de5b66a8d65cc1
UsamaA7med <osamaahmedwork1@gmail.com>
2025-04-02 20:37:29 +0200

    change some otp

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -32,7 +32,7 @@ const SendOTPVerificationEmail = asyncMiddleware(async (req, res, next) => {
   const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
   const hashedOTP = await bcrypt.hash(otp, 11);
   const otpVerification = new OTP({
-    email: req.body.email || req.params.email,
+    email: req.body.email || req.params.email || req.user.email,
     otp: hashedOTP,
     expiresIn: new Date(Date.now() + 5 * 60 * 1000),
   });
@@ -40,7 +40,7 @@ const SendOTPVerificationEmail = asyncMiddleware(async (req, res, next) => {
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
-    to: req.body.email || req.params.email,
+    to: req.body.email || req.params.email || req.user.email,
     subject: "OTP Verification",
     html: `<!DOCTYPE html>
 <html>
